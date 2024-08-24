36dadfc2774aac1b0b2cbbf153f88c3d8d767ea2
Steve <stevethebassguy@gmail.com>
2024-08-24 14:48:34 -0500

    added ParentNode with tests.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,11 @@
+from typing import List
+
 class HTMLNode:
     def __init__(self, tag: str=None, value: str=None, children: list=None, props: dict=None):
-        self.tag = tag
-        self.value = value
-        self.children = children
-        self.props = props
+        self.tag = tag or ""
+        self.value = value or ""
+        self.children: list['HTMLNode'] = children or []
+        self.props = props or {}
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})"
@@ -15,14 +17,34 @@ class HTMLNode:
         return " ".join([f'{key}="{value}"' for key, value in self.props.items()])
 
 
+class ParentNode(HTMLNode):
+    def __init__(self, children: List[HTMLNode], tag: str=None, props: dict=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if self.tag == "":
+            raise ValueError("ParentNode must have a tag")
+        if len(self.children) == 0:
+            raise ValueError("ParentNode must have children")
+        
+        html_string = f"<{self.tag}>" if len(self.props) == 0 else f"<{self.tag} {self.props_to_html()}>"
+        for children in self.children:
+            html_string += children.to_html()
+
+        return f"{html_string}</{self.tag}>"
+
+
 class LeafNode(HTMLNode):
     def __init__(self, value: str, tag: str=None, props: dict=None):
         super().__init__(tag, value, None, props)
         
     def to_html(self) -> str:
-        if self.value is None:
+        if self.value == "":
             raise ValueError("LeafNode must have a value")
-        if self.tag is None:
+        if self.tag == "":
             return self.value
+
+        if len(self.props) == 0:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
 
         return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,12 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("div", "This is a div node")
-        self.assertEqual(repr(node), "HTMLNode(div, This is a div node, None, None)") 
+        self.assertEqual(repr(node), "HTMLNode(div, This is a div node, [], {})") 
     
     def test_props_to_html(self):
         cases = [
@@ -72,6 +72,13 @@ class TestLeafNode(unittest.TestCase):
                 },
                 "expected": '<a href="https://www.boot.dev" target="_blank">This is a link with a target</a>'
             },
+            {
+                "params": {
+                    "tag": "b",
+                    "value": "Bolded text"
+                },
+                "expected": '<b>Bolded text</b>'
+            },
         ]
 
         for case in cases:
@@ -83,3 +90,89 @@ class TestLeafNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             node.to_html()
         
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        cases = [
+            {
+                "params": {
+                    "tag": "p",
+                    "children": [
+                        LeafNode("Bolded text", "b"),
+                        LeafNode("Normal text", None),
+                        LeafNode("italicized text", "i"),
+                        LeafNode("Normal text", None)
+                    ]
+                },
+                "expected": '<p><b>Bolded text</b>Normal text<i>italicized text</i>Normal text</p>'
+            },
+            {
+                "params": {
+                    "tag": "div",
+                    "children": [
+                        LeafNode("This is a div node", "p"),
+                        ParentNode(
+                            [
+                                LeafNode("This is a child div node", "p"),
+                                LeafNode("This is another child div node", "p")
+                            ], 
+                            "div"
+                        )
+                    ]
+                },
+                "expected": '<div><p>This is a div node</p><div><p>This is a child div node</p><p>This is another child div node</p></div></div>'
+            },
+            {
+                "params": {
+                    "tag": "a",
+                    "children": [
+                        LeafNode("This is a link", None)
+                    ],
+                    "props": {"href": "https://www.boot.dev"}
+                },
+                "expected": '<a href="https://www.boot.dev">This is a link</a>'
+            },
+            {
+                "params": {
+                    "tag": "div",
+                    "children": [
+                        LeafNode("This is a link", "a", {"href": "https://www.boot.dev"}), 
+                    ],
+                    "props": {"class": "container"}
+                },
+                "expected": '<div class="container"><a href="https://www.boot.dev">This is a link</a></div>'
+            },
+        ]
+
+        for case in cases:
+            node = ParentNode(**case["params"])
+            self.assertEqual(node.to_html(), case["expected"])
+        
+    def test_to_html_raises_value_error(self):
+        cases = [
+            {
+                "tag": "div",
+                "children": None
+            },
+            {
+                "tag": "div",
+                "children": []
+            },
+            {
+                "tag": None,
+                "children": [LeafNode("This is a div node", "p")]
+            },
+            {
+                "tag": "div",
+                "children": [ParentNode([], "div")]
+            },
+            {
+                "tag": "div",
+                "children": [ParentNode([LeafNode("This is a child div node", "p")], None)]
+            },
+        ]
+
+        for case in cases:
+            node = ParentNode(**case)
+            with self.assertRaises(ValueError):
+                node.to_html()
