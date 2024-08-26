278a2abac2ee4629f5a6777aa76b419d2ea807b0
Steve <stevethebassguy@gmail.com>
2024-08-25 21:14:42 -0500

    modified main to clear public & copy form static

--- src/main.py
+++ src/main.py
@@ -1,5 +1,41 @@
-from textnode import TextNode
+import os
+import shutil
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    refresh_dir('public')
+    copy_static_to_public()
+
+def copy_static_to_public():
+    if not os.path.exists('static'):
+        raise FileNotFoundError('static directory not found')
+    if not os.path.exists('public'):
+        os.mkdir('public')
+    
+    def copy(src, dest):
+        if os.path.isdir(src):
+            if not os.path.exists(dest):
+                print(f'Creating {dest}')
+                os.mkdir(dest)
+            for file in os.listdir(src):
+                copy(os.path.join(src, file), os.path.join(dest, file))
+        else:
+            print(f'Copying {src} to {dest}')
+            shutil.copy(src, dest)
+
+    copy('static', 'public')
+    
+def refresh_dir(dir: str):
+    if os.path.exists(dir):
+        print(f'Clearing {dir}')
+        for file in os.listdir(dir):
+            path = os.path.join(dir, file)
+            if os.path.isdir(path):
+                refresh_dir(path)
+            else:
+                print(f'Removing {path}')
+                os.remove(path)
+    else:
+        print(f'Creating {dir}')
+        os.mkdir(dir)
+
+main()
